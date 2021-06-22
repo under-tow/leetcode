71595181cf00c948056d74a0014ea2030cdfd79f
HopeStation <1035729652@qq.com>
2021-06-23 00:07:04 +0800

    code-6 我的解

--- src/top/hopestation/string/Code_6.java
+++ src/top/hopestation/string/Code_6.java
@@ -17,46 +17,79 @@ public class Code_6 {
 
     public static void main(String[] args) {
 
-        String str = "123456789";
-        int rows = 5;
-        convert_Mine(str,rows);
+        String str = "PAYPALISHIRING";
+        int rows = 3;
+        System.out.println("convert_Mine(str,rows) = " + convert_Mine(str, rows));
     }
 
     /**
      * 我的解
      *  思路：按题中的描述，认为用数组来处理比较合适，
-     *  先放进数组、然后横向读取
+     *  先放进数组、然后横向读取.
+     *  提交LeetCode提示超出时间限制
      * @param s
      * @param numRows
      * @return
      */
     public static String convert_Mine(String s, int numRows) {
 
+
+        if(numRows==1){
+            return s;
+        }
+
         char[] chars = s.toCharArray();
 //        int width = chars.length / numRows;
-        int[][] map = new int[numRows][chars.length];
+        char[][] map = new char[numRows][chars.length];
 
         int x=0,y = 0;
         boolean isForward = true;
+        boolean isNext =false;
         for (int idx = 0; idx < chars.length; idx++) {
 
             map[x][y] = chars[idx];
-            if(x==0){
+            //x
+            if(x==0 && idx!=0){
                 isForward = true ;
-            }else if(x==numRows){
+                isNext = !isNext;
+            }else if(x==numRows-1){
                 isForward = false;
+                isNext = !isNext;
             }
             x = isForward ? x+1 : x-1 ;
+            //y
+            if(isNext){
+                y++;
+            }
 
         }
         printIntMap(map);
-        return "";
+        return getZ(map);
     }
 
-    public static void printIntMap(int [][] map){
+    /**
+     * 获取字符地图中的Z型结果集
+     * @param map
+     * @return
+     */
+    public static String getZ(char [][] map){
+        String s = "";
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[i].length; j++) {
-                System.out.print(" " + map[i][j] + " ");
+                s+= map[i][j];
+            }
+        }
+        return s.replaceAll(  "\u0000", ""  );
+    }
+
+    /**
+     * 打印char数组
+     * @param map
+     */
+    public static void printIntMap(char [][] map){
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                System.out.print("-" + map[i][j]);
             }
             System.out.println();
         }
